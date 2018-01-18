9865fa8d58cd7442a6784404d1ade67e2819226e
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-18 03:34:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badges=[]
+  for x in array
+    badges << badge_maker(x)
+  end
+
+  badges
+end
+
+def assign_rooms(array)
+  i = 1
+  rooms = []
+  for x in array
+    rooms << "Hello, #{x}! You'll be assigned to room #{i}!"
+  end
+  rooms
+end
+
+    
